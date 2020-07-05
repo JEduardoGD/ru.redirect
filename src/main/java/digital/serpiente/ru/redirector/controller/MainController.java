a4d0b455dcfbf3065521238a66a6b76bd1b62310
Eduardo Gutierrez <jgutierrezd@syesoftware.com>
2020-07-05 12:14:20 -0500

    adding db connect parameters

--- src/main/java/digital/serpiente/ru/redirector/controller/MainController.java
+++ src/main/java/digital/serpiente/ru/redirector/controller/MainController.java
@@ -1,5 +1,6 @@
 package digital.serpiente.ru.redirector.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
 	@GetMapping("/{callsign}")
-	void all(@PathVariable String callsign, HttpServletResponse httpServletResponse) {
-		System.out.println(callsign);
-		
-
+	void all(@PathVariable String callsign,
+			HttpServletRequest request,
+			HttpServletResponse httpServletResponse) {
 	    httpServletResponse.setHeader("Location", "http://www.yahoo.com");
 	    httpServletResponse.setStatus(302);
 	}
