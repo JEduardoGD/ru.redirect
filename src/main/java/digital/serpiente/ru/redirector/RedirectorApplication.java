dda4e0d401ca36431b9bcc0456f1cc96810c7cd3
Eduardo Gutierrez <jgutierrezd@syesoftware.com>
2020-07-05 09:20:32 -0500

    first commit

---
+++ src/main/java/digital/serpiente/ru/redirector/RedirectorApplication.java
@@ -0,0 +1,13 @@
+package digital.serpiente.ru.redirector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectorApplication.class, args);
+	}
+
+}
