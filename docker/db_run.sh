7ada3228d3a00a6d28368109d64ea49aeb89ce77
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2021-04-03 14:18:00 -0600

    adjusting last

--- docker/db_run.sh
+++ docker/db_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f DockercomposeDb.yml up
+docker-compose -f DockercomposeDb.yml --env-file /home/ubuntu/vars.env up -d
 #docker volume rm $(docker volume ls -qf dangling=true)
